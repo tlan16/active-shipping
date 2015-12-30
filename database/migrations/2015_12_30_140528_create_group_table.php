b8363b1a291988880ae2d18a65f526dd9a72a933
helin16 <helin16@gmail.com>
2015-12-31 01:06:48 +1100

    added migration scripts for tables

---
+++ database/migrations/2015_12_30_140528_create_group_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('group');
+    }
+}
