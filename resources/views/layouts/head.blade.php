7aae454ce5d79fb545dac9776743bb8abc3039f5
helin16 <helin16@gmail.com>
2016-01-01 22:28:04 +1100

    added default layouts

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,14 @@
+<section id='header'>
+	<div class="top-menu">
+		<div class="col-xs-4 header-left-div">
+			<a href="#" class="">
+				<i class="fa fa-bars"></i>
+				<span class="hidden-xs hidden-sm">Groups</span>
+			</a>
+		</div>
+		<div class="col-xs-4 text-center">
+			<a href='/' class=''>@yield('appName')</a>
+		</div>
+		<div class="col-xs-4 header-right-div text-right">fdsfds</div>
+	</div>
+</section>
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,22 @@
+@section('appName', 'My App Name')
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<title>App Name - @yield('appName')</title>
+</head>
+<body>
+	@section('header')
+		@include('layouts.head')
+	@show
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
