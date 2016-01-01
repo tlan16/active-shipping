48e20cacf0f512b2658a64f4824afe0869b3145d
helin16 <helin16@gmail.com>
2016-01-01 22:52:38 +1100

    added anjularJs

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -12,3 +12,50 @@
 		<div class="col-xs-4 header-right-div text-right">fdsfds</div>
 	</div>
 </section>
+
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span> 
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span
+					class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">@yield('appName')</a>
+		</div>
+		<div id="navbar" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<li>
+					<a href="#">
+						<i class="fa fa-bars"></i>
+						<span class="hidden-sm">Groups</span>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<i class="fa fa-cog"></i>
+						<span class="hidden-sm">Profile</span>
+					</a>
+				</li>
+				<li>
+					<a href="#" title="notification">
+						<i class="fa fa-bell-o"></i>
+						<span class="hidden-sm hidden-md hidden-lg">Notificaton</span>
+					</a>
+				</li>
+				<li>
+					<a href="#" title="logout">
+						<i class="fa fa-sign-out"></i>
+						<span class="hidden-sm hidden-md hidden-lg">Logout</span>
+					</a>
+				</li>
+			</ul>
+			<form class="navbar-form navbar-right">
+				<input type="text" class="form-control" placeholder="Search...">
+			</form>
+		</div>
+	</div>
+</nav>
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -16,7 +16,16 @@
 	@section('header')
 		@include('layouts.head')
 	@show
+	@section('body')
+		<div class="container">
+			<div ng-view></div>
+		</div>
+	@show
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min.js"></script>
+    <script src="/js/app.js"></script>
+    @section('end-js')
+    @show
 </body>
 </html>
